72d9b2f610780236744586b16e84f216b020afa9
Leonardo Rente <leonardo_rente@hotmail.com>
2017-04-19 14:15:41 -0300

    cmd changed

--- SPCodeRun.Cmd/Program.cs
+++ SPCodeRun.Cmd/Program.cs
@@ -4,7 +4,19 @@ using System;
 namespace SPCodeRun.Cmd {
     class Program {
         static void Main(string[] args) {
-            var context = new Context("https://globosatdesenv.sharepoint.com/sites/portalcomercial-dev/pt-br", "leonardo.rente@globosatdesenv.com.br", "Le0n@rDo");
+            GetListItemsFromSiteListsCSOM("https://leonardorente.sharepoint.com/sites/MySite", "leonardo.rente@company.onmicrosoft.com.br", "*****");
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Get All items from all list on SharePoint Site
+        /// </summary>
+        /// <param name="sharepointSiteUrl">SharePoint Online Site URL</param>
+        /// <param name="email">Office 365 Email</param>
+        /// <param name="password">Office 365 Password</param>
+        private static void GetListItemsFromSiteListsCSOM(string sharepointSiteUrl, string email, string password) {
+            var context = new Context(sharepointSiteUrl, email, password);
             var lists = context.GetLists();
 
             Console.WriteLine($"## Lists ##");
@@ -16,11 +28,8 @@ namespace SPCodeRun.Cmd {
                 var items = context.GetListItems(list.Title);
                 foreach (var item in items) {
                     Console.WriteLine($"      - Item Id: {item.Id}");
-                    Console.WriteLine($"      - -------------------------------------");
                 }
             }
-
-            Console.ReadKey();
         }
     }
 }
